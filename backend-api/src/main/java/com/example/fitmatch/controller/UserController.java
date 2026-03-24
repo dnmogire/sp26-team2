043ae7ad4720ca2eeb013197ee193a4b029fcc97
gis3ll3-m <g_marentes@uncg.edu>
2026-03-24 01:16:30 -0400

    Added search, filter by price, and booking history feature.

--- backend-api/src/main/java/com/example/fitmatch/controller/UserController.java
+++ backend-api/src/main/java/com/example/fitmatch/controller/UserController.java
@@ -1,19 +1,37 @@
 package com.example.fitmatch.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.fitmatch.model.Booking;
+import com.example.fitmatch.model.TrainerProfile;
 import com.example.fitmatch.model.User;
 import com.example.fitmatch.repository.UserRepository;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
+import com.example.fitmatch.service.BookingService;
+import com.example.fitmatch.service.TrainerProfileService;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     private final UserRepository userRepo;
+    private final TrainerProfileService trainerProfileService;
+    private final BookingService bookingService;
 
-    public UserController(UserRepository userRepo) {
+    public UserController(UserRepository userRepo, TrainerProfileService trainerProfileService, BookingService bookingService) {
         this.userRepo = userRepo;
+        this.trainerProfileService = trainerProfileService;
+        this.bookingService = bookingService;
     }
 
     // GET /api/users
@@ -54,4 +72,25 @@ public class UserController {
         userRepo.deleteById(id);
         return ResponseEntity.noContent().build();
     }
+
+    // GET      search trainer by activity
+    @GetMapping("/trainers/search")
+    public List <TrainerProfile> searchByActivity(@RequestParam String activity){
+        return trainerProfileService.searchBySpecialty(activity);
+    }
+
+    //GET      filter by price
+    @GetMapping("/trainers/filter")
+    public List <TrainerProfile> filterByPrice(@RequestParam Double maxPrice){
+        return trainerProfileService.searchByMaxRate(maxPrice);
+    }
+
+
+    //GET     booking history
+    @GetMapping("/{id}/bookings")
+    public List <Booking> getBookingHistory(@PathVariable Long id){
+        return bookingService.getByClient(id);
+    }
+
+    
 }
