bd42cb983f231b281c756c18d2f46495382f77e3
gis3ll3-m <g_marentes@uncg.edu>
2026-05-01 21:11:05 -0400

    Added method needed for reviews.

--- mvc-app/src/main/java/com/example/fitmatch/repository/ReviewRepository.java
+++ mvc-app/src/main/java/com/example/fitmatch/repository/ReviewRepository.java
@@ -1,11 +1,14 @@
 package com.example.fitmatch.repository;
 
-import com.example.fitmatch.model.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.fitmatch.model.Review;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByTrainerId(Long trainerId);
+    List<Review> findByClientId(Long clientId);
     Optional<Review> findByBookingId(Long bookingId);
 }
